97128c68f13dd66681e24c8f96c9c67bcb65ace5
Disha Bajaj <dishabajaj1508@gmail.com>
2024-01-09 12:14:54 +0530

    Created Routes

--- src/components/AppRouters.js
+++ src/components/AppRouters.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { MoviePage } from "../pages/MoviePage";
+import { PageNotFound } from "../pages/PageNotFound";
 
 export const AppRoutes = () => {
   return (
@@ -8,7 +9,10 @@ export const AppRoutes = () => {
       <Routes>
         <Route path="/" element={MoviePage} />
         <Route path="/movies/top-rated" element={MoviePage} />
-        <Route path="/movies/top-rated" element={MoviePage} />
+        <Route path="/movies/popular" element={MoviePage} />
+        <Route path="/movies/upcoming" element={MoviePage} />
+        <Route path="/movies/:id" element={MoviePage} />
+        <Route path="*" element={PageNotFound} />
       </Routes>
     </>
   );
