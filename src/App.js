90994a84f142cbb137d704cc2f5228ba3d6f6705
Disha Bajaj <dishabajaj1508@gmail.com>
2024-01-09 15:51:48 +0530

    Done with Footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import { AppRoutes } from "./components/AppRoutes";
 import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <AppRoutes />
+      <Footer />
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react";
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer class="fixed bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow p-6 dark:bg-slate-900 dark:border-slate-50">
+      <p class="text-sm text-center text-gray-00 sm:text-center dark:text-gray-400">
+        © {new Date().getFullYear() + " "}
+        {/*hange url once hosted*/}
+        <a
+          href="http://sl-filmfest.netlify.app/"
+          class="hover:text-primary-800"
+        >
+          FilmFiesta &trade;
+        </a>
+        .All Rights Reserved
+      </p>
+    </footer>
+  );
+};
