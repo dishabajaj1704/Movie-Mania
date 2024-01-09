25ffe84db8396adccb7aae6b3cea4bff8ce4fcf1
Disha Bajaj <dishabajaj1508@gmail.com>
2024-01-09 12:27:10 +0530

    chore:Imported AppRoutes in App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import { AppRoutes } from "./components/AppRoutes";
 
 function App() {
   return (
     <div className="App">
-      
+      <AppRoutes />
     </div>
   );
 }
