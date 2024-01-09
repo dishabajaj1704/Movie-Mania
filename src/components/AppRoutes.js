cf624ce9028c0cc7251237ff5b9d061d7fe6b7bd
Disha Bajaj <dishabajaj1508@gmail.com>
2024-01-09 12:25:54 +0530

    fix:Route for MovieDetails

--- src/components/AppRoutes.js
+++ src/components/AppRoutes.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import { MovieDetailsPage } from "../pages/MovieDetailsPage";
 import { MoviePage } from "../pages/MoviePage";
 import { PageNotFound } from "../pages/PageNotFound";
 
@@ -7,12 +8,12 @@ export const AppRoutes = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={MoviePage} />
-        <Route path="/movies/top-rated" element={MoviePage} />
-        <Route path="/movies/popular" element={MoviePage} />
-        <Route path="/movies/upcoming" element={MoviePage} />
-        <Route path="/movies/:id" element={MoviePage} />
-        <Route path="*" element={PageNotFound} />
+        <Route path="/" element={<MoviePage />} />
+        <Route path="/movies/top-rated" element={<MoviePage />} />
+        <Route path="/movies/popular" element={<MoviePage />} />
+        <Route path="/movies/upcoming" element={<MoviePage />} />
+        <Route path="/movies/:id" element={<MovieDetailsPage />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </>
   );
