7ad561e6738ba9465dd4a5eeb3c73c9517c2220f
Disha Bajaj <dishabajaj1508@gmail.com>
2024-01-12 16:43:14 +0530

    Done with Customized PageNotFound page

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,7 +1,33 @@
-import React from 'react'
-
+import React from "react";
+import NotFoundImage from "../assests/pageNotFound.jpg";
+import { Link } from "react-router-dom";
 export const PageNotFound = () => {
   return (
-    <div>PageNotFound</div>
-  )
-}
+    <main>
+      <section className="flex flex-col justify-center px-2 mt-5">
+        <div className="flex flex-col items-center py-4">
+          <h2 className="text-7xl text-slate-800 dark:text-slate-50 font-medium">
+            You seem to be Lost!
+          </h2>
+          <p className="text-3xl text-slate-800 dark:text-slate-50">
+            404 Page Not Found
+          </p>
+          <div className="max-w-lg mt-14">
+            <img className="rounded" src={NotFoundImage} alt="Not Found" />
+          </div>
+        </div>
+        <div className="flex justify-center py-4">
+          <Link
+            to="/"
+            className="inline-flex items-center px-5 py-2 
+text-sm font-medium text-center text-white bg-primary-800 
+rounded-lg hover:bg-primary-1000 focus:outline-none dark:bg-primary-800 
+dark:hover:bg-primary-1000"
+          >
+            Back To Home!
+          </Link>
+        </div>
+      </section>
+    </main>
+  );
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 import { MovieDetailsPage } from "./MovieDetailsPage";
 import { MoviePage } from "./MoviePage";
 import { PageNotFound } from "./PageNotFound";
+import { SearchPage } from "./SearchPage";
