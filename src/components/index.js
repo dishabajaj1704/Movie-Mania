1b9fc0785f3f5addb97b5c67f1ad62a523030f57
Disha Bajaj <dishabajaj1508@gmail.com>
2024-01-12 18:12:08 +0530

    fix:ScrollToTop Problem

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ import { Header } from "./Header";
 import { Footer } from "./Footer";
 import { MovieCard } from "./MovieCard";
 import { AppRoutes } from "./AppRouters";
+import { ScrollToTop } from "./ScrollToTop";
