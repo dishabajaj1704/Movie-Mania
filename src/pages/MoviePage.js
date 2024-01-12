52db71fd7c1f729abec86175173593586d84d38e
Disha Bajaj <dishabajaj1508@gmail.com>
2024-01-12 15:10:44 +0530

    Showing MovieCardSkeleton on Loading

--- src/pages/MoviePage.js
+++ src/pages/MoviePage.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { MovieCard } from "../components/MovieCard";
+import { MovieCardSkeleton } from "../components/MovieCardSkeleton";
 import useFetch from "../hooks/useFetch";
 
 export const MoviePage = ({ apiPath }) => {
@@ -8,9 +9,18 @@ export const MoviePage = ({ apiPath }) => {
   useEffect(() => {
     setUrl(`${BASE_API}${apiPath}?api_key=${process.env.REACT_APP_API_KEY}`);
   }, [apiPath]);
+
+  function renderSkeletons(count) {
+    const skeletons = [];
+    for (let i = 1; i <= count; i++) {
+      skeletons.push(<MovieCardSkeleton key={i} />);
+    }
+    return skeletons;
+  }
   return (
     <main>
       <div className="flex flex-wrap justify-start">
+        {isLoading && renderSkeletons(6)}
         {movies &&
           movies.results.map((movie) => (
             <MovieCard movie={movie} key={movie.id} />
