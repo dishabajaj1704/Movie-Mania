1dd445c1902fbfcb71a467f8825ad1c916a343a0
Disha Bajaj <dishabajaj1508@gmail.com>
2024-01-12 14:34:48 +0530

    Added dependency in useEffect

--- src/pages/MoviePage.js
+++ src/pages/MoviePage.js
@@ -7,7 +7,7 @@ export const MoviePage = ({ apiPath }) => {
   const { data: movies, error, isLoading, setUrl } = useFetch();
   useEffect(() => {
     setUrl(`${BASE_API}${apiPath}?api_key=79ad8f4a680df3224a502863cd4eebfb`);
-  });
+  },[apiPath]);
   return (
     <main>
       <div className="flex flex-wrap justify-start">
