b69b70a460f96dd2c085a7e135efe841b511e2f2
Disha Bajaj <dishabajaj1508@gmail.com>
2024-03-17 03:28:38 +0530

    fix

--- src/hooks/useDynamicTitle.js
+++ src/hooks/useDynamicTitle.js
@@ -3,6 +3,9 @@ import React, { useEffect } from "react";
 export const useDynamicTitle = (title, defaultTitle = "Film FIesta") => {
   useEffect(() => {
     document.title = title;
+
+    //cleanup code
+    //In this case, the cleanup function resets the document.title to the defaultTitle when the component is unmounted
     return () => {
       document.title = defaultTitle;
     };
